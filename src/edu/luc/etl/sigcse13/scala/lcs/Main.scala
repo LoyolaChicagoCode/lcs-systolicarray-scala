dbaf40b420041d3c4619716b5b94c4ee6ed3307b
Konstantin Läufer <laufer@cs.luc.edu>
2013-02-20 15:18:03 -0600

    initial import

---
+++ src/edu/luc/etl/sigcse13/scala/lcs/Main.scala
@@ -0,0 +1,29 @@
+package edu.luc.etl.sigcse13.scala.lcs
+
+import scala.actors.Actor
+import scala.actors.Actor._
+
+object Main extends App {
+
+  println("hello")
+  val root = new Node(0, 0, 5, 5)
+  root.start()
+  root ! "lkj"
+  root ! "lkj"
+  root ! "lkj"
+  root ! "lkj"
+}
+
+class Node(row: Int, col: Int, n: Int, m: Int) extends Actor {
+
+  require { 0 <= row && row < n && 0 <= col && col < m }
+
+  override def act() {
+    println("starting (" + row + ", " + col + ")")
+    react {
+      case _ => {
+        println("pong")
+      }
+    }
+  }
+}
