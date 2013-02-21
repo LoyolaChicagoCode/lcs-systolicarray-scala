9593b9b1c1d3e58be13d606610e05b57554748a2
Konstantin Läufer <laufer@cs.luc.edu>
2013-02-20 23:17:51 -0600

    first shot at wiring the entire array together

--- src/edu/luc/etl/sigcse13/scala/lcs/Main.scala
+++ src/edu/luc/etl/sigcse13/scala/lcs/Main.scala
@@ -4,29 +4,63 @@ import scala.actors.Actor
 import scala.actors.Actor._
 
 object Main extends App {
-
-  // TODO will probably need lazy 2d array of actors so they
-  // can find each other more easily (graph, not tree!)
-
   println("hello")
-  val root = new Node(0, 0, 5, 5)
+  val root = SystolicArray(3, 5)
   root.start()
-  root ! "lkj"
-  root ! "lkj"
-  root ! "lkj"
-  root ! "lkj"
+  (0 until 5) foreach { i =>
+    Thread.sleep(3000)
+    println("---round " + i + "----")
+    root ! "lkj"
+  }
 }
 
-class Node(row: Int, col: Int, n: Int, m: Int) extends Actor {
+object SystolicArray {
+  def apply(n: Int, m: Int) = {
+    require { 0 < n }
+    require { 0 < m }
+    lazy val a: Stream[Stream[Node]] = Stream.tabulate(n, m) {
+      (i, j) => new Node(i, j, n, m, a)
+    }
+    a(0)(0)
+  }
+}
 
-  require { 0 <= row && row < n && 0 <= col && col < m }
+class Node(row: Int, col: Int, n: Int, m: Int, a: => Stream[Stream[Node]]) extends Actor {
+
+  require { 0 <= row && row < n }
+  require { 0 <= col && col < m }
+
+  println("creating (" + row + ", " + col + ")")
 
   override def act() {
     println("starting (" + row + ", " + col + ")")
-    react {
-      case _ => {
-        println("pong")
+    var start = true
+    loop {
+      println("waiting  (" + row + ", " + col + ")")
+      barrier(if (row == 0 || col == 0) 1 else 3) {
+        () => { propagate(start) ; start = false }
       }
+      // one-way message: anything below here is skipped!
     }
+  }
+
+  protected def barrier(n : Int)(p: () => Unit) {
+    if (n <= 0)
+      p()
+    else
+      react { case _ => barrier(n - 1)(p) }
+    // one-way message: anything after react is skipped!
+  }
+
+  protected def propagate(start: Boolean) {
+    if (start) {
+      if (row < n - 1)                a(row + 1)(col    ).start()
+      if (col < m - 1)                a(row    )(col + 1).start()
+      if (row < n - 1 && col < m - 1) a(row + 1)(col + 1).start()
+    }
+    println("firing   (" + row + ", " + col + ")")
+    if (row < n - 1)                a(row + 1)(col    ) ! "lkj"
+    if (col < m - 1)                a(row    )(col + 1) ! "lkj"
+    if (row < n - 1 && col < m - 1) a(row + 1)(col + 1) ! "lkj"
   }
 }
