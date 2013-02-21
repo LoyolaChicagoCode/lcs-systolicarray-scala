4bf7d341fdc3406b74c05d807e0c414042e5b992
Konstantin Läufer <laufer@cs.luc.edu>
2013-02-21 12:59:30 -0600

    parameterized array with common function and introduced SyncVar for result

--- src/edu/luc/etl/sigcse13/scala/lcs/Main.scala
+++ src/edu/luc/etl/sigcse13/scala/lcs/Main.scala
@@ -2,65 +2,81 @@ package edu.luc.etl.sigcse13.scala.lcs
 
 import scala.actors.Actor
 import scala.actors.Actor._
+import scala.concurrent.SyncVar
 
 object Main extends App {
+  import SystolicArray.Pos
   println("hello")
-  val root = SystolicArray(3, 5)
+  val result = new SyncVar[Int]
+  val root = SystolicArray(3, 5, (p: Pos, ms: Map[Pos, Int]) => ms.values.sum, result)
   root.start()
-  (0 until 5) foreach { i =>
-    Thread.sleep(3000)
-    println("---round " + i + "----")
-    root ! "lkj"
-  }
+  root ! ((-1, -1) -> 1)
+  println("end result = " + result.take)
 }
 
 object SystolicArray {
-  def apply(n: Int, m: Int) = {
+
+  type LazyArray[T] = Stream[Stream[Node[T]]]
+
+  type Pos = (Int, Int)
+
+  type Acc[T] = (Pos, Map[Pos, T]) => T
+
+  def apply[T](n: Int, m: Int, f: Acc[T], result: SyncVar[T]) = {
     require { 0 < n }
     require { 0 < m }
-    lazy val a: Stream[Stream[Node]] = Stream.tabulate(n, m) {
-      (i, j) => new Node(i, j, n, m, a)
+    lazy val a: LazyArray[T] = Stream.tabulate(n, m) {
+      (i, j) => new Node(i, j, n, m, a, f, result)
     }
     a(0)(0)
   }
-}
 
-class Node(row: Int, col: Int, n: Int, m: Int, a: => Stream[Stream[Node]]) extends Actor {
+  class Node[T](row: Int, col: Int, rows: Int, cols: Int, a: => LazyArray[T],
+      f: Acc[T], result: SyncVar[T]) extends Actor {
 
-  require { 0 <= row && row < n }
-  require { 0 <= col && col < m }
+    require { 0 <= row && row < rows }
+    require { 0 <= col && col < cols }
 
-  println("creating (" + row + ", " + col + ")")
+    println("creating (" + row + ", " + col + ")")
 
-  override def act() {
-    println("starting (" + row + ", " + col + ")")
-    var start = true
-    loop {
-      println("waiting  (" + row + ", " + col + ")")
-      barrier(if (row == 0 || col == 0) 1 else 3) {
-        () => { propagate(start) ; start = false }
+    override def act() {
+      println("starting (" + row + ", " + col + ")")
+      var start = true
+      loop {
+        println("waiting  (" + row + ", " + col + ")")
+        barrier(if (row == 0 || col == 0) 1 else 3) { ms =>
+          if (start) { startNeighbors() ; start = false }
+          propagate(ms)
+        }
+        // one-way message: anything below here is skipped!
       }
-      // one-way message: anything below here is skipped!
     }
-  }
 
-  protected def barrier(n : Int)(p: () => Unit) {
-    if (n <= 0)
-      p()
-    else
-      react { case _ => barrier(n - 1)(p) }
-    // one-way message: anything after react is skipped!
-  }
+    protected def barrier(n : Int)(p: Map[Pos, T] => Unit): Unit =
+      barrier1(n)(p)(Map.empty)
 
-  protected def propagate(start: Boolean) {
-    if (start) {
-      if (row < n - 1)                a(row + 1)(col    ).start()
-      if (col < m - 1)                a(row    )(col + 1).start()
-      if (row < n - 1 && col < m - 1) a(row + 1)(col + 1).start()
+    protected def barrier1(n : Int)(p: Map[Pos, T] => Unit)(ms: Map[Pos, T]): Unit = {
+      if (n <= 0)
+        p(ms)
+      else
+        react { case m: (Pos, T) => barrier1(n - 1)(p)(ms + m) }
+      // one-way message: anything after react is skipped!
     }
-    println("firing   (" + row + ", " + col + ")")
-    if (row < n - 1)                a(row + 1)(col    ) ! "lkj"
-    if (col < m - 1)                a(row    )(col + 1) ! "lkj"
-    if (row < n - 1 && col < m - 1) a(row + 1)(col + 1) ! "lkj"
+
+    protected def startNeighbors() {
+      if (row < rows - 1)                   a(row + 1)(col    ).start()
+      if (col < cols - 1)                   a(row    )(col + 1).start()
+      if (row < rows - 1 && col < cols - 1) a(row + 1)(col + 1).start()
+    }
+
+    protected def propagate(ms: Map[Pos, T]) {
+      val r = f((row, col), ms)
+      val m = (row, col) -> r
+      println("firing   " + m)
+      if (row < rows - 1)                     a(row + 1)(col    ) ! m
+      if (col < cols - 1)                     a(row    )(col + 1) ! m
+      if (row < rows - 1 && col < cols - 1)   a(row + 1)(col + 1) ! m
+      if (row >= rows - 1 && col >= cols - 1) result.set(r)
+    }
   }
 }
